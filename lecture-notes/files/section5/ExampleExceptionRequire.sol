e19b69f9b164af9550411e4bed33e06a3af177f6
B4ker <voiceee54@gmail.com>
2025-10-26 12:59:01 -0400

    Pushing require()

---
+++ lecture-notes/files/section5/ExampleExceptionRequire.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.0;
+
+contract ExamplExceptionRequire {
+
+    mapping(address => uint) public balanceReceived;
+
+    function recieveMoney() public payable {
+        balanceReceived[msg.sender]+=msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(_amount<=balanceReceived[msg.sender], "Not enough Funds, aborting");
+        balanceReceived[msg.sender]-=_amount;
+        _to.transfer(_amount);
+        
+    }
+}
